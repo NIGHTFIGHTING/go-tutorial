2584ab94d6e1db56b94d5ed386953feea69d9d51
liuqi15 <liuqi15@baidu.com>
2018-12-01 18:52:19 +0800

    add for

--- 2/2_4.go
+++ 2/2_4.go
@@ -5,6 +5,27 @@ import (
     "fmt"
 )
 
+func grade(score int) string {
+    g := ""
+    switch {
+    case score < 0 || score > 100:
+        panic(fmt.Sprintf(
+            "Wrong score: %d", score))
+    case score < 60:
+        g = "F"
+    case score < 80:
+        g = "C"
+    case score < 90:
+        g = "B"
+    case score <= 100:
+        g = "A"
+    default:
+        panic(fmt.Sprintf(
+            "Wrong score: %d", score))
+    }
+    return g
+}
+
 func main() {
     const filename = "abc.txt"
     contents, err := ioutil.ReadFile(filename)
@@ -13,4 +34,9 @@ func main() {
     } else {
         fmt.Printf("%s\n", contents)
     }
+
+    fmt.Println(grade(10))
+    fmt.Println(grade(90))
+    fmt.Println(grade(1000))
+    fmt.Println(grade(10))
 }
