5cb05f35d73ae21a979262cea8165a05c9e27934
liuqi15 <liuqi15@baidu.com>
2018-12-02 17:30:25 +0800

    add3_2

--- 3/3_2.go
+++ 3/3_2.go
@@ -37,5 +37,14 @@ func main() {
     fmt.Println(s2)
     s2 = s2[2:]
     fmt.Println(s2)
-    
+
+    fmt.Println("Extending slice")
+    arr[0], arr[2] = 0, 2
+    fmt.Println("arr =", arr)
+    s1 = arr[2:6]
+    s2 = s1[3:5] // [s1[3], s1[4]]
+    fmt.Printf("s1=%v, len(s1)=%d, cap(s1)=%d\n",
+            s1, len(s1), cap(s1))
+    fmt.Printf("s2=%v, len(s2)=%d, cap(s2)=%d\n",
+            s2, len(s2), cap(s2))
 }
